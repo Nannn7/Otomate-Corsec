2630438010e79b16020f9cd069391e30b7540331
Daeng Deni Mardaeni <ddeni05@gmail.com>
2024-07-30 14:40:13 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Installation
+
+1. Install the required Node.js packages by running `npm install` in the root directory of your Laravel project.
+
+2. Build the assets by running `npm run dev` in the root directory of your Laravel project.
+
+3. Start the Laravel development server by running `php artisan serve`.
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+	<!-- begin: container -->
+	<div class="container-fixed">
+		<div class="flex flex-col md:flex-row justify-center md:justify-between items-center gap-3 py-5">
+			<div class="flex order-2 md:order-1 gap-2 font-normal text-2sm">
+				<span class="text-gray-500">
+					2024©
+				</span>
+				<a class="text-gray-600 hover:text-primary" href="https://arthagraha.com">
+					PT Bank Artha Graha Internasional Tbk
+				</a>
+			</div>
+			<nav class="flex order-1 md:order-2 gap-4 font-normal text-2sm text-gray-600">
+				<a class="hover:text-primary" href="">
+					Docs
+				</a>
+				<a class="hover:text-primary"
+					href="#">
+					FAQ
+				</a>
+				<a class="hover:text-primary" href="#">
+					Support
+				</a>
+                <a class="hover:text-primary" href="#">
+                    Developer
+                </a>
+			</nav>
+		</div>
+	</div>
+	<!-- end: container -->
+</footer>
