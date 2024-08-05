8bfc23fa23f12e19ac56b569d33deb08d650b0c7
Daeng Deni Mardaeni <ddeni05@gmail.com>
2024-08-05 14:24:31 +0700

    Feature #2 : Breadcrumbs

---
+++ resources/views/layouts/breadcrumbs.blade.php
@@ -0,0 +1,19 @@
+@unless ($breadcrumbs->isEmpty())
+    <div class="flex [.header_&amp;]:below-lg:hidden items-center gap-1.25 text-xs lg:text-sm font-medium mb-2.5 lg:mb-0" data-reparent="true" data-reparent-mode="prepend|lg:prepend" data-reparent-target="#content_container|lg:#header_container">
+        @foreach ($breadcrumbs as $breadcrumb)
+            @if (!is_null($breadcrumb->url) && !$loop->last)
+                <li class="breadcrumb-item"></li>
+                <span class="text-gray-600">
+                    <a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a>
+                </span>
+                <i class="ki-filled ki-right text-gray-500 text-3xs">
+                </i>
+            @else
+                <span class="text-gray-700">
+                    {{ $breadcrumb->title }}
+                </span>
+            @endif
+
+        @endforeach
+    </div>
+@endunless
