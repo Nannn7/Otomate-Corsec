b545c0c85bdd1067778bd9eb1051901e3b9fcc9a
Daeng Deni Mardaeni <ddeni05@gmail.com>
2024-08-22 11:49:32 +0700

    update global selectize

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,18 +3,19 @@ import "./bootstrap";
 import "../metronic/core/index";
 import "../metronic/app/layouts/base.js";
 
-import $ from 'jquery';
+import $ from "jquery";
 
 window.jQuery = $;
 window.$ = $;
 
-
 import TomSelect from "tom-select";
 
-let settings = {
-    plugins: ['dropdown_input'],
-    create: false,
-    createOnBlur: true
-};
+document.querySelectorAll(".tomselect").forEach((el) => {
+    let settings = {
+        plugins: ["dropdown_input"],
+        create: false,
+        createOnBlur: true,
+    };
 
-new TomSelect('.tomselect', settings);
+    new TomSelect(el, settings);
+});
