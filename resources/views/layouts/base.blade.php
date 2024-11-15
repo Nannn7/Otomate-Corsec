0a917cc597c8ebaf7dc43a8214544283601b2763
rahmatrafli1 <rahmatrafli1@gmail.com>
2024-11-15 13:52:17 +0700

    Menambahkan gambar pdf dan mengubah pesan error supaya kita tahu pesannya apa

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,12 +1,12 @@
 @php
     use Nwidart\Modules\Module;
 @endphp
-    <!doctype html>
+<!doctype html>
 <html class="h-full" data-theme="true" data-theme-mode="light" lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>{{ env('APP_NAME')  }}</title>
+    <title>{{ env('APP_NAME') }}</title>
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
@@ -16,44 +16,44 @@
 
 <body style="zoom:80%!important" class="flex h-full metronic sidebar-fixed header-fixed bg-[#fefefe] dark:bg-coal-500">
 
-@if (session('error'))
-    <em class="hidden toastr" data-type="error" data-message=" {{ session('status') }}"></em>
-@endif
+    @if (session('error'))
+        <em class="hidden toastr" data-type="error" data-message=" {{ session('error') }}"></em>
+    @endif
 
-@if (session('info'))
-    <em class="hidden toastr" data-type="info" data-message=" {{ session('info') }}"></em>
-@endif
+    @if (session('info'))
+        <em class="hidden toastr" data-type="info" data-message=" {{ session('info') }}"></em>
+    @endif
 
-@if (session('success'))
-    <em class="hidden toastr" data-type="success" data-message=" {{ session('success') }}"></em>
-@endif
+    @if (session('success'))
+        <em class="hidden toastr" data-type="success" data-message=" {{ session('success') }}"></em>
+    @endif
 
 
-<!--begin::Theme mode setup on page load-->
-<script>
-    const defaultThemeMode = 'system'; // light|dark|system
-    let themeMode;
+    <!--begin::Theme mode setup on page load-->
+    <script>
+        const defaultThemeMode = 'system'; // light|dark|system
+        let themeMode;
 
-    if (document.documentElement) {
-        if (localStorage.getItem('theme')) {
-            themeMode = localStorage.getItem('theme');
-        } else if (document.documentElement.hasAttribute('data-theme-mode')) {
-            themeMode = document.documentElement.getAttribute('data-theme-mode');
-        } else {
-            themeMode = defaultThemeMode;
+        if (document.documentElement) {
+            if (localStorage.getItem('theme')) {
+                themeMode = localStorage.getItem('theme');
+            } else if (document.documentElement.hasAttribute('data-theme-mode')) {
+                themeMode = document.documentElement.getAttribute('data-theme-mode');
+            } else {
+                themeMode = defaultThemeMode;
+            }
+
+            if (themeMode === 'system') {
+                themeMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+            }
+
+            document.documentElement.classList.add(themeMode);
         }
+    </script>
+    <!--end::Theme mode setup on page load-->
 
-        if (themeMode === 'system') {
-            themeMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        }
-
-        document.documentElement.classList.add(themeMode);
-    }
-</script>
-<!--end::Theme mode setup on page load-->
-
-@yield('main')
-@stack('scripts')
+    @yield('main')
+    @stack('scripts')
 
 
 </body>
