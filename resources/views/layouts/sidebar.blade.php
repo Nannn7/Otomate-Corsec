eae476b2ec2b3b8075a1b7b05baa18c6e05aeb39
Daeng Deni Mardaeni <ddeni05@gmail.com>
2024-12-29 21:14:24 +0700

    Refactor penanganan menu pada sidebar
    - Mengurutkan tampilan menu berdasarkan section priority.
    - Menambahkan validasi untuk memastikan `$menus` didefinisikan.
    - Merapikan logika iterasi dan heading dengan pendekatan yang lebih efisien.

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -39,95 +39,106 @@
                     </div>
                 </div>
                 @php
-                    $headingSystem = 0;
+                    $headingOtorisasi = 0;
                     $headingMain = 0;
                     $headingMaster = 0;
+                    $headingSystem = 0;
+
+                    // Ensure $menus is defined and is an object
+                    $menus = isset($menus) ? json_decode(json_encode($menus)) : new stdClass;
+
+                    // Define the order of sections
+                    $sectionOrder = ['main','otorisator', 'master', 'system'];
                 @endphp
-                @php $menus = json_decode(json_encode($menus)); @endphp
-                @foreach($menus as $key => $value)
-                    @foreach($value as $menu)
-                        @if($key=='main')
-                            @if($headingMain == 0)
-                                <div class="menu-item pt-2.25 pb-px">
-                                    <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
-                                        Apps
-                                    </span>
-                                </div>
-                                @php $headingMain = 1; @endphp
-                            @endif
-                        @elseif($key=='master')
-                            @if($headingMaster == 0)
-                                <div class="menu-item pt-2.25 pb-px">
-                                    <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
-                                        Master Data
-                                    </span>
-                                </div>
-                                @php $headingMaster = 1; @endphp
-                            @endif
-                        @elseif($key=='system')
-                            @if($headingSystem == 0)
-                                <div class="menu-item pt-2.25 pb-px">
-                                    <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
-                                        Systems
-                                    </span>
-                                </div>
-                                @php $headingSystem = 1; @endphp
-                            @endif
+
+                @foreach($sectionOrder as $section)
+                    @if(!empty($menus->$section))
+                        @if($section == 'otorisator' && $headingOtorisasi == 0)
+                            <div class="menu-item pt-2.25 pb-px">
+                                <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
+                                    Otorisator
+                                </span>
+                            </div>
+                            @php $headingOtorisasi = 1; @endphp
+                        @elseif($section == 'main' && $headingMain == 0)
+                            <div class="menu-item pt-2.25 pb-px">
+                                <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
+                                    Apps
+                                </span>
+                            </div>
+                            @php $headingMain = 1; @endphp
+                        @elseif($section == 'master' && $headingMaster == 0)
+                            <div class="menu-item pt-2.25 pb-px">
+                                <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
+                                    Master Data
+                                </span>
+                            </div>
+                            @php $headingMaster = 1; @endphp
+                        @elseif($section == 'system' && $headingSystem == 0)
+                            <div class="menu-item pt-2.25 pb-px">
+                                <span class="menu-heading uppercase text-2sm font-semibold text-gray-500 pl-[10px] pr-[10px]">
+                                    Systems
+                                </span>
+                            </div>
+                            @php $headingSystem = 1; @endphp
                         @endif
-                        @if(auth()->user()->hasRole($menu->roles))
-                            @if(isset($menu->sub))
-                                <div class="menu-item {{ request()->routeIs($menu->path) || request()->routeIs($menu->path.'.*') ? 'show' : '' }}" data-menu-item-toggle="accordion" data-menu-item-trigger="click">
-                                    <div
-                                        class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]"
-                                        tabindex="0">
-                                        <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px]">
-                                            <i class="{{ $menu->icon ?? 'ki-filled ki-element-11 text-lg' }}"></i>
-                                        </span>
-                                        <span class="menu-title text-sm font-semibold text-gray-700 menu-item-active:text-primary menu-link-hover:!text-primary">
-                                            {{ $menu->title }}
-                                        </span>
-                                        <span class="menu-arrow text-gray-400 w-[20px] shrink-0 justify-end ml-1 mr-[-10px]">
-                                            <i class="ki-filled ki-plus text-2xs menu-item-show:hidden">
-                                            </i>
-                                            <i class="ki-filled ki-minus text-2xs hidden menu-item-show:inline-flex">
-                                            </i>
-                                        </span>
-                                    </div>
-                                    @if(is_array($menu->sub))
-                                        <div class="menu-accordion gap-0.5 pl-[10px] relative before:absolute before:left-[20px] before:top-0 before:bottom-0 before:border-l before:border-gray-200">
-                                            @foreach($menu->sub as $sub)
-                                                @if(auth()->user()->hasRole($sub->roles))
-                                                    <div class="menu-item {{ request()->routeIs($sub->path.'.*') && in_array(request()->route()->getName(), [$sub->path.'.index', $sub->path.'.create', $sub->path.'.edit']) ? 'active' : '' }}">
-                                                        <a class="menu-link gap-[14px] pl-[10px] pr-[10px] py-[8px] border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg"
-                                                           href="{{ $sub->path ? route($sub->path.'.index') : '' }}" tabindex="0">
-                                                            <span
-                                                                class="menu-bullet flex w-[6px] relative before:absolute before:top-0 before:size-[6px] before:rounded-full before:-translate-x-1/2 before:-translate-y-1/2 menu-item-active:before:bg-primary menu-item-hover:before:bg-primary">
-                                                            </span>
-                                                            <span
-                                                                class="menu-title text-2sm font-medium text-gray-700 menu-item-active:text-primary menu-item-active:font-semibold menu-link-hover:!text-primary">
-                                                                {{ $sub->title  }}
-                                                            </span>
-                                                        </a>
-                                                    </div>
-                                                @endif
-                                            @endforeach
+
+                        @foreach($menus->$section as $menu)
+                            @if(auth()->user()->hasRole($menu->roles))
+                                @if(isset($menu->sub))
+                                    <div class="menu-item {{ request()->routeIs($menu->path) || request()->routeIs($menu->path.'.*') ? 'show' : '' }}" data-menu-item-toggle="accordion" data-menu-item-trigger="click">
+                                        <div
+                                            class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]"
+                                            tabindex="0">
+                                            <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px]">
+                                                <i class="{{ $menu->icon ?? 'ki-filled ki-element-11 text-lg' }}"></i>
+                                            </span>
+                                            <span class="menu-title text-sm font-semibold text-gray-700 menu-item-active:text-primary menu-link-hover:!text-primary">
+                                                {{ $menu->title }}
+                                            </span>
+                                            <span class="menu-arrow text-gray-400 w-[20px] shrink-0 justify-end ml-1 mr-[-10px]">
+                                                <i class="ki-filled ki-plus text-2xs menu-item-show:hidden">
+                                                </i>
+                                                <i class="ki-filled ki-minus text-2xs hidden menu-item-show:inline-flex">
+                                                </i>
+                                            </span>
                                         </div>
-                                    @endif
-                                </div>
-                            @else
-                                <div class="menu-item {{ request()->routeIs($menu->path.'.*') ? 'active' : '' }}">
-                                    <a class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]" href="{{ $menu->path ? route($menu->path.'.index') : '' }}">
-                                        <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px] menu-item-active:text-primary menu-link-hover:!text-primary">
-                                            <i class="{{ $menu->icon ?? 'ki-filled ki-element-11 text-lg' }}"></i>
-                                        </span>
-                                        <span class="menu-title text-sm font-semibold text-gray-700 menu-item-active:text-primary menu-link-hover:!text-primary">
-                                            {{ $menu->title }}
-                                        </span>
-                                    </a>
-                                </div>
+                                        @if(is_array($menu->sub))
+                                            <div class="menu-accordion gap-0.5 pl-[10px] relative before:absolute before:left-[20px] before:top-0 before:bottom-0 before:border-l before:border-gray-200">
+                                                @foreach($menu->sub as $sub)
+                                                    @if(auth()->user()->hasRole($sub->roles))
+                                                        <div class="menu-item {{ request()->routeIs($sub->path.'.*') && in_array(request()->route()->getName(), [$sub->path.'.index', $sub->path.'.create', $sub->path.'.edit']) ? 'active' : '' }}">
+                                                            <a class="menu-link gap-[14px] pl-[10px] pr-[10px] py-[8px] border border-transparent items-center grow menu-item-active:bg-secondary-active dark:menu-item-active:bg-coal-300 dark:menu-item-active:border-gray-100 menu-item-active:rounded-lg hover:bg-secondary-active dark:hover:bg-coal-300 dark:hover:border-gray-100 hover:rounded-lg"
+                                                               href="{{ $sub->path ? route($sub->path.'.index') : '' }}" tabindex="0">
+                                                                <span
+                                                                    class="menu-bullet flex w-[6px] relative before:absolute before:top-0 before:size-[6px] before:rounded-full before:-translate-x-1/2 before:-translate-y-1/2 menu-item-active:before:bg-primary menu-item-hover:before:bg-primary">
+                                                                </span>
+                                                                <span
+                                                                    class="menu-title text-2sm font-medium text-gray-700 menu-item-active:text-primary menu-item-active:font-semibold menu-link-hover:!text-primary">
+                                                                    {{ $sub->title  }}
+                                                                </span>
+                                                            </a>
+                                                        </div>
+                                                    @endif
+                                                @endforeach
+                                            </div>
+                                        @endif
+                                    </div>
+                                @else
+                                    <div class="menu-item {{ request()->routeIs($menu->path.'.*') ? 'active' : '' }}">
+                                        <a class="menu-link flex items-center grow cursor-pointer border border-transparent gap-[10px] pl-[10px] pr-[10px] py-[6px]" href="{{ $menu->path ? route($menu->path.'.index') : '' }}">
+                                            <span class="menu-icon items-start text-gray-500 dark:text-gray-400 w-[20px] menu-item-active:text-primary menu-link-hover:!text-primary">
+                                                <i class="{{ $menu->icon ?? 'ki-filled ki-element-11 text-lg' }}"></i>
+                                            </span>
+                                            <span class="menu-title text-sm font-semibold text-gray-700 menu-item-active:text-primary menu-link-hover:!text-primary">
+                                                {{ $menu->title }}
+                                            </span>
+                                        </a>
+                                    </div>
+                                @endif
                             @endif
-                        @endif
-                    @endforeach
+                        @endforeach
+                    @endif
                 @endforeach
             </div>
         </div>
