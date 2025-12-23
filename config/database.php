710819d3eef31f599f8af323a4bf28a3f7b020ea
Nanda Surya <114451345+Nannn7@users.noreply.github.com>
2025-12-23 10:57:26 +0700

    Create Table

--- config/database.php
+++ config/database.php
@@ -84,9 +84,10 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'schema' => env('DB_SCHEMA', 'public'),
+            'database' => env('DB_DATABASE', 'mombod-db'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
@@ -144,7 +145,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,9 +1,9 @@
 @php
     use Illuminate\Support\Facades\Route;
 
-    $route      = explode('.',Route::currentRouteName());
-	$modules    = file_get_contents(dirname(__FILE__, 4) . '/modules_statuses.json');
-    $module     = json_decode($modules);
+    $route = explode('.', Route::currentRouteName());
+    $modules = file_get_contents(dirname(__FILE__, 4) . '/modules_statuses.json');
+    $module = json_decode($modules);
 @endphp
 
 @extends('layouts.base', ['module' => $module])
