6faee2138a0f5436477704f50e9bfcb20e6828b4
Nanda Surya <114451345+Nannn7@users.noreply.github.com>
2026-04-29 09:27:58 +0700

    fix(corsec): login page

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>{{ env('APP_NAME') }}</title>
+    <title>{{ config('app.name', 'Corsec App') }}</title>
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/x-icon" href="favicon.ico">
